8a2fe1e829326f1ccb5fd38ccad917201c489012
RulaaHza <146732703+RulaaHza@users.noreply.github.com>
2023-10-10 21:50:08 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,14 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    num =1;
-    while( len(list) < number_of_primes):
-        if num > 1:
-           for i in range(2, num):
-               if (num % i) == 0:
-                   break
-           else:
-               list.append(num)
-        num+=1
-    return list
+    try:
+        list = []
+        num =1
+        if(number_of_primes >0):
+        
+            while( len(list) < number_of_primes):
+                if num > 1:
+                   for i in range(2, num):
+                       if (num % i) == 0:
+                           break
+                   else:
+                       list.append(num)
+                num+=1
+            return list
+    except ValueError:
+        print('Positive number expected for square root operation')
