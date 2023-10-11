497c4a103f97b745be7009ca808451d746cf033d
RulaaHza <146732703+RulaaHza@users.noreply.github.com>
2023-10-11 14:21:55 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,18 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-        list = []
-        num =2
-       if number_of_primes <= 0:
-                raise ValueError('Positive number expected for list size')
-       else:
-            while( len(list) < number_of_primes):
-                if num > 1:
-                   for i in range(2, num):
-                       if (num % i) == 0:
-                           break
-                   else:
-                       list.append(num)
-                num+=1
-            return list
-                
+    if number_of_primes <= 0:
+        raise ValueError('Positive number expected for list size')
+    
+    prime_list = []
+    num = 2
+
+    while len(prime_list) < number_of_primes:
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                prime_list.append(num)
+        num += 1
+
+    return prime_list
